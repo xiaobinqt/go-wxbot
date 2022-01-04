e2f5c8106897c1cb28b3c8da02a4b1b6d6b39aa0
weibin <xiaobinqt@163.com>
2022-01-04 23:38:03 +0800

    update mod

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module go-wxbot
 go 1.16
 
 require (
-	github.com/eatmoreapple/openwechat v1.1.7-0.20211229101158-d193cbd641ea
+	github.com/eatmoreapple/openwechat v1.1.8
 	github.com/json-iterator/go v1.1.12
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 	}
 
 	bot := openwechat.DefaultBot()
-	// bot := openwechat.DefaultBot(openwechat.Desktop) // 桌面模式，上面登录不上的可以尝试切换这种模式
+	//bot := openwechat.DefaultBot(openwechat.Normal) // 桌面模式，上面登录不上的可以尝试切换这种模式
 
 	// 注册消息处理函数
 	bot.MessageHandler = func(msg *openwechat.Message) {
