5b8c67519833843a358f2b01ee0b5f932863f44e
weibin <xiaobinqt@163.com>
2022-09-18 17:24:33 +0800

    add love ticker

--- comm/ticker/love.go
+++ comm/ticker/love.go
@@ -39,13 +39,14 @@ func LoveTicker() {
 			if nowTime == "09:30" {
 				SendMessageToLover("亲爱的，早上好！爱你每一天！\n新的一天从一句土味情话开始：", tian.C_saylove)
 			}
+
 			if nowTime == "10:00" {
 				lz, err := tian.GetMessageV1(tian.C_lizhiguyan)
 				message := ""
 				if err != nil {
-					message = fmt.Sprintf("盛年不重来，一日难再晨。及时当勉励，岁月不待人。\n今年还剩 %d 天。", funcs.RemainingDays())
+					message = fmt.Sprintf("今年还剩 %d 天。\n\n盛年不重来，一日难再晨。及时当勉励，岁月不待人。", funcs.RemainingDays())
 				} else {
-					message = fmt.Sprintf("%s\n\n今年还剩 %d 天。", lz, funcs.RemainingDays())
+					message = fmt.Sprintf("今年还剩 %d 天。\n\n%s", funcs.RemainingDays(), lz)
 				}
 
 				err = global.WxFriends.
@@ -56,9 +57,31 @@ func LoveTicker() {
 					logrus.Error(err.Error())
 				}
 			}
+
 			if nowTime == "23:00" {
 				SendMessageToLover("亲爱的，11 点了，该洗漱睡觉了！\n临睡之际送你一句土味情话：", tian.C_saylove)
 			}
+
+			if t.Weekday() >= 1 && t.Weekday() <= 5 {
+				if nowTime == "09:55" {
+					global.WxFriends.
+						SearchByRemarkName(1, global.Conf.Keys.HoneyLove).
+						SendText("虽然我们都不爱上班，但是还是不要忘记上报打卡。")
+				}
+
+				if nowTime == "20:00" {
+					global.WxFriends.
+						SearchByRemarkName(1, global.Conf.Keys.HoneyLove).
+						SendText("八点了，该下班了，记得打卡。")
+				}
+
+				if nowTime == "21:00" {
+					global.WxFriends.
+						SearchByRemarkName(1, global.Conf.Keys.HoneyLove).
+						SendText("九点了，要是还没下班，真的要准备下班了，记得打卡。")
+				}
+			}
+
 		}
 	}
 }
