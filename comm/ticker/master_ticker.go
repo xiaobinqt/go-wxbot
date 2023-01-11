ff93652cf35fac76bb3d6cd273ac277c650342fa
weibin <xiaobinqt@163.com>
2023-01-11 10:33:01 +0800

    add 背单词

--- comm/ticker/master_ticker.go
+++ comm/ticker/master_ticker.go
@@ -36,6 +36,17 @@ func MasterTicker() {
 				}
 			}
 
+			if nowTime == "22:00" {
+				message := "记得背单词兄弟，别一天天的想偷懒！"
+				err := global.WxFriends.
+					SearchByRemarkName(1, global.Conf.Keys.MasterAccount).
+					SendText(message)
+				if err != nil {
+					err = errors.Wrapf(err, "SendMessageToMasterAccout err")
+					logrus.Error(err.Error())
+				}
+			}
+
 			if nowTime == "23:00" {
 				message := "休息一下，整理一下今天的账单吧！记日记的时间也到了，不要忘记了哦！"
 				err := global.WxFriends.
